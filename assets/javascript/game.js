ca5dd8c53562f5d8fde267351c3f219ed0f07e41
Pooja Chauhan <PoojaNiteshChauhan.gmail>
2019-05-24 13:30:10 -0400

    updating

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var computerChoices = ["Tiger", "Elephant", "Deer" ,"Giraffe", "Bears"];
+
+alert (" got here");
+
+document.onkeyup = function(event) {
+
+var userGuess = event.key;
+
+console.log(userGuess);
+}
+
+
+
