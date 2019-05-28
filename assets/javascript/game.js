82a793bb241f7ab842ef43e743be4912293eefe5
Pooja Chauhan <PoojaNiteshChauhan.gmail>
2019-05-27 23:14:55 -0400

    updating homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,77 @@
-var computerChoices = ["Tiger", "Elephant", "Deer" ,"Giraffe", "Bears"];
+var computerChoices = ["tiger", "Elephant", "Deer" ,"Giraffe", "Bear"];
+var wrongLetter =[];
+var userWord = [];
+var userGuess =" "
+var roundCounter =0;
+var userScore = 0;
 
-alert (" got here");
 
-document.onkeyup = function(event) {
 
-var userGuess = event.key;
+function usserAnswer(userChoice)
+{
+    document.getElementById("already-guessed").innerText = userChoice;
+} 
 
-console.log(userGuess);
+function createDashes(animalName)
+{
+    var showDash= " ";
+    for (var i= 0; i < animalName.length ; i++)
+    {
+        showDash = showDash + "-";
+    }
+    document.getElementById("Answer").innerText = showDash;
 }
 
+function fillword(lengthOfWord)
+{
+    for (var i= 0; i < lengthOfWord ; i++)
+    {
+        userWord.push("-");
+    }
+}
+
+createDashes (computerChoices[roundCounter]);
 
 
+function findletter(Guess)
+{
+    var currentWord = computerChoices[roundCounter];
+    
+    console.log(currentWord);
+    console.log(currentWord.indexOf(Guess))
+    console.log(Guess);
+
+
+        if (currentWord.indexOf(Guess) != -1)
+            {
+                userWord[currentWord.indexOf(Guess)]=Guess;
+                document.getElementById("Answer").innerText = userWord;
+                console.log(userWord);               
+            }
+        else 
+            {
+                wrongLetter.push(Guess);
+                document.getElementById("already-guessed").innerText = wrongLetter;
+
+            }
+
+    
+}
+
+function playGame()
+{
+    fillword(computerChoices[roundCounter].length);
+    document.onkeyup = function(event) 
+    {
+        userGuess = event.key;
+        findletter(userGuess);
+    }
+    
+    
+    
+
+
+
+}
+
+playGame();
