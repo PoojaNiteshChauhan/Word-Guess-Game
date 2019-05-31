1aab758f11c787dba0a0f1e38dca66380158463f
Chauhan <569875@bah.com>
2019-05-31 16:57:55 -0400

    Updated Code

--- assets/javascript/gameF.js
+++ assets/javascript/gameF.js
@@ -1,4 +1,4 @@
-var computerChoices = ["tiger", "Elephant", "Deer" ,"Giraffe", "Bear"];
+var computerChoices = ["tiger", "bear", "rat" ,"monkey", "dog"];
 var wrongAnswer =[];
 var Answer = [];
 var wins = 0;
@@ -9,48 +9,75 @@ var userLetter = ""
 
 
 function restGame (){
-    guessesLeft = 0;
     wrongAnswer =[];
     Answer = [];
     document.getElementById("Result").textContent = ""
     createDashes(computerChoices[animalName]);
+    console.log (computerChoices[animalName])
+    console.log (computerChoices[animalName].length)
+    guessesLeft = computerChoices[animalName].length + 5;
 }
 
 function createDashes(animal)
 {
     var showDash= " ";
     for (var i= 0; i < animal.length ; i++)
-
     {
-        showDash = showDash + "-";
+        Answer[i] = "-";
     }
-    document.getElementById("Answer").innerText = showDash;
+    document.getElementById("Answer").innerText = Answer.join("");
 }
 
 function playGame (){
     restGame();
     document.onkeyup = function(event) 
     {
-        userLetter = event.key;
-        if ((guessesLeft > 0) && (computerChoices[animalName] != Answer) )
-        {
-
         
-        }
-        else if (computerChoices[animalName] === Answer)
+        userLetter = event.key;
+        if (guessesLeft <= 0 )
         {
-            win++;
-            restGame();
+            losses++;
             updateScoreBoard();
-            document.getElementById("Result").innerText = " You Win "; 
+            document.getElementById("Result").textContent = " You Lose Press Any Key Continue"; 
+            animalName = Math.floor(Math.random()*computerChoices.length);
+            restGame();
         }
+        else if ((guessesLeft > 0) && (computerChoices[animalName] != Answer.join("")) )
+        {
+            
+            if (computerChoices[animalName].indexOf(userLetter )!= -1)
+            {
+                Answer[computerChoices[animalName].indexOf(userLetter )]=userLetter;
+                document.getElementById("Answer").textContent = Answer.join("");
+                guessesLeft--;
+                if (computerChoices[animalName] === Answer.join(''))
+                {
+                    wins++;
+                    updateScoreBoard();
+                    document.getElementById("Result").textContent = " You Win Press Any Key Continue"; 
+                    animalName = Math.floor(Math.random()*computerChoices.length);
+                    restGame();
+                }               
+            }
+            else
+            {
+                if (wrongAnswer.indexOf(userLetter)=== -1)
+                    {
+                        wrongAnswer.push(userLetter);
+                    }
+                document.getElementById("wrongAnswer").textContent = wrongAnswer;              
+                guessesLeft--;
+                
+            }       
+        }
+        
     }
 
 }
 
 function updateScoreBoard (){
-document.getElementById("win").innerText = " Wins: " + wins;
-document.getElementById("losses").innerText = " Loss: " + losses;  
+    document.getElementById("Win").textContent = " Wins: " + wins;
+    document.getElementById("Losses").textContent = " Loss: " + losses;  
 }
 playGame ();
 
